c71c4a688ddf25c820d8103eff138ee795549fe1
antoniaSHorta <142348629+antoniaSHorta@users.noreply.github.com>
2023-09-07 12:21:43 -0300

    Add files via upload

---
+++ CliniSync/src/main/java/com/mycompany/clinisync/CliniSync.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clinisync;
+
+/**
+ *
+ * @author asali
+ */
+public class CliniSync {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
